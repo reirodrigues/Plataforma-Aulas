bcba3a9fe8636d68489af5a00f6e50facc98e077
Reinaldo Rodrigues <reinaldorodrigues13@yahoo.com>
2022-06-27 15:38:13 -0300

    update interface/type

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -8,7 +8,7 @@ interface LessonProps {
   title: string;
   slug: string;
   availableAt: Date;
-  type: 'Live' | 'Class';
+  type: 'live' | 'class';
 }
 
 export function Lesson(props: LessonProps) {
